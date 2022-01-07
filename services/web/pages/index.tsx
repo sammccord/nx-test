3bb1160b1073d1150422348d402892e395e708bd
Sam McCord <sam.mccord@protonmail.com>
2022-01-06 17:23:53 -0700

    test a cache breaking change for 1/2 packages

--- services/web/pages/index.tsx
+++ services/web/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Small change. Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
